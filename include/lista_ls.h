efd7725174353fc15315164df66e6b72912b0754
Maximiano Leite <macio.pb.cg@gmail.com>
2020-04-30 12:03:24 -0300

    function for insert

--- include/lista_ls.h
+++ include/lista_ls.h
@@ -3,6 +3,8 @@
 #include <cs50.h>
 
 #define MAX_USUARIO 100
+#define FALSE 0
+#define TRUE 1
 
 typedef struct t_lista_ls Lista_ls;
 typedef char boolean;
@@ -16,5 +18,8 @@ Usuario;
 
 Lista_ls *criar_lista_ls();
 void apagar_lista_ls(Lista_ls *li);
+boolean inserir_no_inicio_da_lista(Lista_ls *li, Usuario us);
+boolean inserir_no_fim_da_lista(Lista_ls *li, Usuario us);
+boolean inserir_na_posicao(Lista_ls *li, Usuario us, int pos);
 
 #endif
--- src/lista_ls.c
+++ src/lista_ls.c
@@ -6,14 +6,30 @@
 struct t_lista_ls
 {
     int qtd;
-    Usuario dados;
+    Usuario dados[MAX_USUARIO];
 };
+
+boolean lista_cheia(Lista_ls *li)
+{
+    if(li->qtd == MAX_USUARIO)
+        return TRUE;
+
+    return FALSE;
+}
+
+boolean ponteiro_esta_nulo(Lista_ls *li)
+{
+    if (li == NULL)
+        return TRUE;
+
+    return FALSE;
+}
 
 Lista_ls *criar_lista_ls(void)
 {
     Lista_ls *li = (Lista_ls *)malloc(sizeof(Lista_ls));
 
-    if (li != NULL)
+    if (!ponteiro_esta_nulo(li))
         li->qtd = 0;
 
     return li;
@@ -21,6 +37,45 @@ Lista_ls *criar_lista_ls(void)
 
 void apagar_lista_ls(Lista_ls *li)
 {
-    if(li != NULL)
+    if(ponteiro_esta_nulo(li))
         free(li);
+}
+
+boolean inserir_no_inicio_da_lista(Lista_ls *li, Usuario us)
+{
+    if(ponteiro_esta_nulo(li) || lista_cheia(li))
+        return FALSE;
+    int i;
+
+    for(i = li->qtd - 1; i >= 0; i++)
+        li->dados[i+1] = li->dados[i];
+
+    li->dados[i] = us;
+    li->qtd++;
+
+    return TRUE;
+}
+
+boolean inserir_no_fim_da_lista(Lista_ls *li, Usuario us)
+{
+    if(ponteiro_esta_nulo(li) || lista_cheia(li))
+        return FALSE;
+    li->dados[li->qtd] = us;
+    li->qtd++;
+
+    return TRUE;
+}
+
+boolean inserir_na_posicao(Lista_ls *li, Usuario us, int pos)
+{
+    if(ponteiro_esta_nulo(li) || lista_cheia(li) || pos <= 0)
+        return FALSE;
+    if(pos > li->qtd)
+        return FALSE;
+    int i;
+    for (i = li->qtd - 1; i>= pos; i--)
+        li->dados[i+1] = li->dados[i];
+
+    li->dados[i] = us;
+    return TRUE;
 }
